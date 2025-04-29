1e24d3d281a41a39108deafe8cd0abf7f3b244dc
Juanma <noctiferium@gmail.com>
2025-04-29 16:05:38 -0500

    Organizando index y redirección

--- src/crud/urls.py
+++ src/crud/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('Iniciar-Sesion', views.sign_in, name='sign_in'),
     path('Registro/', views.sign_up, name='sign_up'),
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('eliminar/<str:idProducto>', views.eliminar , name='eliminar_producto'),
     path('agregar_productos', views.agregar_productos, name='agregar_productos'),
